d5237c50ea2310cae37427bc59a6fe721d351979
Michel Albert <michel@albert.lu>
2011-12-31 15:55:42 +0100

    Slightly more abstract model

--- muninbrowser/model.py
+++ muninbrowser/model.py
@@ -25,3 +25,98 @@ class Munin(object):
         return sorted([_ for _ in self.__config[groupname].keys() if not
             _.startswith('__')])
 
+class MuninObject(object):
+
+    def bind(self, config):
+        self._conf = config
+
+class Group(MuninObject):
+    """
+    A class representing a group of hosts
+    """
+
+    def __init__(self, name, conf=None):
+        self.name = name
+        if conf:
+            self.bind(conf)
+
+    def __repr__(self):
+        return "<%s %s>" % (
+                self.__class__.__name__,
+                self.name
+                )
+
+    def __eq__(self, other):
+        return isinstance(other, Group) and (
+                other.name == self.name)
+
+    def __hash__(self):
+        return self.name.__hash__()
+
+    @classmethod
+    def all(self, conf):
+        "retrieve all groups"
+
+        out = []
+        for group in conf.members:
+            tmp = Group(group, conf=conf)
+            out.append(tmp)
+
+        return out
+
+    @classmethod
+    def get(self, conf, name):
+        "get one group by name"
+        if name not in conf.members:
+            return None
+        return Group(name, conf=conf)
+
+    @property
+    def hosts(self):
+        out = []
+        for host in self._conf[self.name].members:
+            tmp = Host(self.name, host, self._conf)
+            out.append(tmp)
+        return out
+
+class Host(MuninObject):
+    """
+    A class representing a monitored host
+    """
+
+    def __init__(self, group, name, conf=None):
+        self.group = group
+        self.name = name
+        if conf:
+            self.bind(conf)
+
+    def __repr__(self):
+        return "<%s %s[%s]>" % (
+                self.__class__.__name__,
+                self.group,
+                self.name
+                )
+
+    def __eq__(self, other):
+        return isinstance(other, Host) and (
+                other.name == self.name and
+                other.group == self.group)
+
+    def __hash__(self):
+        return self.group.__hash__() ^ self.name.__hash__()
+
+    @classmethod
+    def all(self, conf, groupname=None):
+        "retrieve all hosts"
+
+        out = []
+        if groupname:
+            for host in conf[groupname].members:
+                tmp = Host(groupname, host, conf=conf)
+                out.append(tmp)
+        else:
+            for group in conf.members:
+                for host in conf[group].members:
+                    tmp = Host(group, host, conf=conf)
+                    out.append(tmp)
+        return out
--- tests/model.py
+++ tests/model.py
@@ -1,24 +1,49 @@
 import unittest2 as unittest
-from muninbrowser.model import Munin
+from muninbrowser.model import Group, Host
 
-class TestModel(unittest.TestCase):
+class TestGroup(unittest.TestCase):
 
     def setUp(self):
+        from muninbrowser.configparser import read
         from os.path import join, dirname
         conf_name = join(dirname(__file__), 'test_config.conf')
-        self._model = Munin(conf_name)
+        self._conf = read(open(conf_name))
 
     def test_groups(self):
-        "test retrieval of host groups"
-        expected = set(['hostgroup', 'localdomain', 'testgroup'])
-        self.assertEquals(set(self._model.groups), expected)
+        "Retrieving all groups"
+        expected = set([
+            Group('testgroup'),
+            Group('hostgroup'),
+            Group('localdomain'),
+            ])
+        self.assertEquals(set(Group.all(self._conf)), expected)
 
     def test_hosts(self):
-        "test retrieval of hosts"
-        self.assertEquals(self._model.hosts('testgroup'), [])
-        self.assertEquals(self._model.hosts('localdomain'), ['localhost.localdomain'])
-        self.assertEquals(self._model.hosts('hostgroup'), ['hostname'])
+        "Retrieving hosts from a group"
+        group = Group('localdomain', self._conf)
+        expected = [Host('localdomain', 'localhost.localdomain', conf=self._conf)]
+        self.assertEquals(group.hosts, expected)
 
-    def test_graphs(self):
-        pass
+    def test_get_group(self):
+        expected = Group('localdomain')
+        self.assertEquals(Group.get(self._conf, 'localdomain'), expected)
 
+class TestHosts(unittest.TestCase):
+
+    def setUp(self):
+        from muninbrowser.configparser import read
+        from os.path import join, dirname
+        conf_name = join(dirname(__file__), 'test_config.conf')
+        self._conf = read(open(conf_name))
+
+    def test_hosts(self):
+        expected = set([
+            Host('hostgroup', 'hostname'),
+            Host('localdomain', 'localhost.localdomain')
+            ])
+        self.assertEquals(set(Host.all(self._conf)), expected)
+
+        expected = set([
+            Host('localdomain', 'localhost.localdomain')
+            ])
+        self.assertEquals(set(Host.all(self._conf, 'localdomain')), expected)
